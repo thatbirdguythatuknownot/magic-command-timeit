22a857735b29bfbddc4bead16fc686b514cfe919
thatbirdguythatuknownot <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2021-07-01 15:16:57 +0800

    Fixed possibly all NameErrors
    Added new functions, imports, and variables to fix NameErrors

--- implementations.py
+++ implementations.py
@@ -1,8 +1,34 @@
-import timeit, time, gc, itertools, shlex, getopt, ast, traceback, sys, math, pstats
+import timeit, time, gc, itertools, shlex, getopt, ast, traceback, sys, math, pstats, re, os, io, tempfile
 import cProfile as profile
 from pathlib import Path
-from io import StringIO
-#Below check copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/utils/timing.py#L23-L67
+from shutil import get_terminal_size as _get_terminal_size
+StringIO = io.StringIO
+
+esc_re = re.compile(r"(\x1b[^m]+m)")
+#Below check completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/core/page.py#L324-L345
+if os.name == 'nt' and os.environ.get('TERM','dumb') != 'emacs':
+    import msvcrt
+    def page_more():
+        """ Smart pausing between pages
+        @return:    True if need print more lines, False if quit
+        """
+        sys.stdout.write('---Return to continue, q to quit--- ')
+        ans = msvcrt.getwch()
+        if ans in ("q", "Q"):
+            result = False
+        else:
+            result = True
+        sys.stdout.write("\b"*37 + " "*37 + "\b"*37)
+        return result
+else:
+    def page_more():
+        ans = input('---Return to continue, q to quit--- ')
+        if ans.lower().startswith('q'):
+            return False
+        else:
+            return True
+
+#Below check completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/utils/timing.py#L23-L67
 try:
     import resource
     def clocku():
@@ -43,6 +69,51 @@ except ImportError:
         This just returns perf_counter() and zero."""
         return time.perf_counter(),0.0
 
+#Below function completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/core/page.py#L81-L124
+def _detect_screen_size(screen_lines_def):
+    """Attempt to work out the number of lines on the screen.
+    This is called by page(). It can raise an error (e.g. when run in the
+    test suite), so it's separated out so it can easily be called in a try block.
+    """
+    TERM = os.environ.get('TERM',None)
+    if not((TERM=='xterm' or TERM=='xterm-color') and sys.platform != 'sunos5'):
+        # curses causes problems on many terminals other than xterm, and
+        # some termios calls lock up on Sun OS5.
+        return screen_lines_def
+    
+    try:
+        import termios
+        import curses
+    except ImportError:
+        return screen_lines_def
+    
+    # There is a bug in curses, where *sometimes* it fails to properly
+    # initialize, and then after the endwin() call is made, the
+    # terminal is left in an unusable state.  Rather than trying to
+    # check every time for this (by requesting and comparing termios
+    # flags each time), we just save the initial terminal state and
+    # unconditionally reset it every time.  It's cheaper than making
+    # the checks.
+    try:
+        term_flags = termios.tcgetattr(sys.stdout)
+    except termios.error as err:
+        # can fail on Linux 2.6, pager_page will catch the TypeError
+        raise TypeError('termios error: {0}'.format(err)) from err
+
+    try:
+        scr = curses.initscr()
+    except AttributeError:
+        # Curses on Solaris may not be complete, so we can't use it there
+        return screen_lines_def
+    
+    screen_lines_real,screen_cols = scr.getmaxyx()
+    curses.endwin()
+
+    # Restore terminal state in case endwin() didn't.
+    termios.tcsetattr(sys.stdout,termios.TCSANOW,term_flags)
+    # Now we have what we needed: the screen size in rows/columns
+    return screen_lines_real
+
 #Below function copied from IPython source code, https://github.com/ipython/ipython/blob/master/IPython/core/magics/execution.py#L1466-L1503
 def _format_time(timespan, precision=3):
     if timespan >= 60.0:
@@ -72,9 +143,184 @@ def _format_time(timespan, precision=3):
         order = 3
     return u"%.*g %s" % (precision, timespan * scaling[order], units[order])
 
+#Below function completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/utils/data.py#L26-L28
+def chop(seq, size):
+    """Chop a sequence into chunks of the given size."""
+    return [seq[i:i+size] for i in range(0,len(seq),size)]
+
+#Below function completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/core/page.py#L285-L304
+def get_pager_cmd(pager_cmd=None):
+    """Return a pager command.
+    Makes some attempts at finding an OS-correct one.
+    """
+    if os.name == 'posix':
+        default_pager_cmd = 'less -R'  # -R for color control sequences
+    elif os.name in ['nt','dos']:
+        default_pager_cmd = 'type'
+    
+    if pager_cmd is None:
+        try:
+            pager_cmd = os.environ['PAGER']
+        except:
+            pager_cmd = default_pager_cmd
+    
+    if pager_cmd == 'less' and '-r' not in os.environ.get('LESS', '').lower():
+        pager_cmd += ' -R'
+    
+    return pager_cmd
+
+#Below function completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/core/page.py#L307-L320
+def get_pager_start(pager, start):
+    """Return the string for paging files with an offset.
+    This is the '+N' argument which less and more (under Unix) accept.
+    """
+    
+    if pager in ['less','more']:
+        if start:
+            start_string = '+' + str(start)
+        else:
+            start_string = ''
+    else:
+        start_string = ''
+    return start_string
+
+#Below function completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/utils/terminal.py#L128-L129
+def get_terminal_size(defaultx=80, defaulty=25):
+    return _get_terminal_size((defaultx, defaulty))
+
+#Below function completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/core/page.py#L57-L79
+def page_dumb(strng, start=0, screen_lines=25):
+    """Very dumb 'pager' in Python, for when nothing else works.
+    Only moves forward, same interface as page(), except for pager_cmd and
+    mode.
+    """
+    if isinstance(strng, dict):
+        strng = strng.get('text/plain', '')
+    out_ln  = strng.splitlines()[start:]
+    screens = chop(out_ln,screen_lines-1)
+    if len(screens) == 1:
+        print(os.linesep.join(screens[0]))
+    else:
+        last_escape = ""
+        for scr in screens[0:-1]:
+            hunk = os.linesep.join(scr)
+            print(last_escape + hunk)
+            if not page_more():
+                return
+            esc_list = esc_re.findall(hunk)
+            if len(esc_list) > 0:
+                last_escape = esc_list[-1]
+        print(last_escape + os.linesep.join(screens[-1]))
+
+#Below function completely copied from IPython source code, https://github.com/ipython/ipython/blob/a2c2a2dcec36224c7dca561efcf9ed9e5c514c3c/IPython/core/page.py#L128-L236
+def page(strng, start=0, screen_lines=0, pager_cmd=None):
+    """Display a string, piping through a pager after a certain length.
+    
+    strng can be a mime-bundle dict, supplying multiple representations,
+    keyed by mime-type.
+    The screen_lines parameter specifies the number of *usable* lines of your
+    terminal screen (total lines minus lines you need to reserve to show other
+    information).
+    If you set screen_lines to a number <=0, page() will try to auto-determine
+    your screen size and will only use up to (screen_size+screen_lines) for
+    printing, paging after that. That is, if you want auto-detection but need
+    to reserve the bottom 3 lines of the screen, use screen_lines = -3, and for
+    auto-detection without any lines reserved simply use screen_lines = 0.
+    If a string won't fit in the allowed lines, it is sent through the
+    specified pager command. If none given, look for PAGER in the environment,
+    and ultimately default to less.
+    If no system pager works, the string is sent through a 'dumb pager'
+    written in python, very simplistic.
+    """
+    
+    # for compatibility with mime-bundle form:
+    if isinstance(strng, dict):
+        strng = strng['text/plain']
+    
+    # Ugly kludge, but calling curses.initscr() flat out crashes in emacs
+    TERM = os.environ.get('TERM','dumb')
+    if TERM in ['dumb','emacs'] and os.name != 'nt':
+        print(strng)
+        return
+    # chop off the topmost part of the string we don't want to see
+    str_lines = strng.splitlines()[start:]
+    str_toprint = os.linesep.join(str_lines)
+    num_newlines = len(str_lines)
+    len_str = len(str_toprint)
+    
+    # Dumb heuristics to guesstimate number of on-screen lines the string
+    # takes.  Very basic, but good enough for docstrings in reasonable
+    # terminals. If someone later feels like refining it, it's not hard.
+    numlines = max(num_newlines,int(len_str/80)+1)
+    
+    screen_lines_def = get_terminal_size()[1]
+    
+    # auto-determine screen size
+    if screen_lines <= 0:
+        try:
+            screen_lines += _detect_screen_size(screen_lines_def)
+        except (TypeError, UnsupportedOperation):
+            print(str_toprint)
+            return
+    
+    #print 'numlines',numlines,'screenlines',screen_lines  # dbg
+    if numlines <= screen_lines :
+        #print '*** normal print'  # dbg
+        print(str_toprint)
+    else:
+        # Try to open pager and default to internal one if that fails.
+        # All failure modes are tagged as 'retval=1', to match the return
+        # value of a failed system command.  If any intermediate attempt
+        # sets retval to 1, at the end we resort to our own page_dumb() pager.
+        pager_cmd = get_pager_cmd(pager_cmd)
+        pager_cmd += ' ' + get_pager_start(pager_cmd,start)
+        if os.name == 'nt':
+            if pager_cmd.startswith('type'):
+                # The default WinXP 'type' command is failing on complex strings.
+                retval = 1
+            else:
+                fd, tmpname = tempfile.mkstemp('.txt')
+                tmppath = Path(tmpname)
+                try:
+                    os.close(fd)
+                    with tmppath.open("wt") as tmpfile:
+                        tmpfile.write(strng)
+                        cmd = "%s < %s" % (pager_cmd, tmppath)
+                    # tmpfile needs to be closed for windows
+                    if os.system(cmd):
+                        retval = 1
+                    else:
+                        retval = None
+                finally:
+                    Path.unlink(tmppath)
+        else:
+            try:
+                retval = None
+                # Emulate os.popen, but redirect stderr
+                proc = subprocess.Popen(pager_cmd,
+                                shell=True,
+                                stdin=subprocess.PIPE,
+                                stderr=subprocess.DEVNULL
+                                )
+                pager = os._wrap_close(io.TextIOWrapper(proc.stdin), proc)
+                try:
+                    pager_encoding = pager.encoding or sys.stdout.encoding
+                    pager.write(strng)
+                finally:
+                    retval = pager.close()
+            except IOError as msg:  # broken pipe when user quits
+                if msg.args == (32, 'Broken pipe'):
+                    retval = None
+                else:
+                    retval = 1
+            except OSError:
+                # Other strange problems, sometimes seen in Win2k/cygwin
+                retval = 1
+        if retval is not None:
+            page_dumb(strng,screen_lines=screen_lines)
+
 #Below function has content copied from IPython source code, https://github.com/ipython/ipython/blob/master/IPython/core/magics/execution.py#L305-L379
 def _run_with_profiler(code, opts, namespace):
-        opts = dict(opts)
         prof = profile.Profile()
         try:
             prof = prof.runctx(code, namespace, namespace)
@@ -82,9 +328,9 @@ def _run_with_profiler(code, opts, namespace):
         except SystemExit:
             sys_exit = """*** SystemExit exception caught in code being profiled."""
         
-        stats = pstats.Stats(prof).strip_dirs().sort_stats(*opts.get('s', ['time']))
+        stats = pstats.Stats(prof).strip_dirs().sort_stats(*opts.get('-s', ['time']))
         
-        lims = opts.get('l', [])
+        lims = opts.get('-l', [])
         if lims:
             lims, lims_ = [], lims  # rebuild lims with ints/floats/strings
             for lim in lims_:
@@ -108,12 +354,12 @@ def _run_with_profiler(code, opts, namespace):
         output = stdout_trap.getvalue()
         output = output.rstrip()
         
-        if 'q' not in opts:
-            page.page(output)
+        if '-q' not in opts:
+            page(output)
         print(sys_exit, end=' ')
         
-        dump_file = opts.D[0]
-        text_file = opts.T[0]
+        dump_file = opts.get('-D', [''])[0]
+        text_file = opts.get('-T', [''])[0]
         if dump_file:
             prof.dump_stats(dump_file)
             print(
@@ -128,7 +374,7 @@ def _run_with_profiler(code, opts, namespace):
                 f"\n*** Profile printout saved to text file {repr(text_file)}.{sys_exit}"
             )
         
-        if 'r' in opts:
+        if '-r' in opts:
             return stats
         
         return None
@@ -237,6 +483,14 @@ class Timer(timeit.Timer):
 #Implementation of %prun
 def magic_prun(parameter_s=''):
     opts, arg_str = getopt.getopt(parameter_s, 'D:l:rs:T:q')
+    optdict = {}
+    for key, value in opts:
+        if key in optdict:
+            optdict[key].append(value)
+        else:
+            optdict[key] = [value]
+    
+    opts = optdict
     return _run_with_profiler(arg_str, opts, globals())
 
 #Implementation of %timeit
@@ -249,12 +503,12 @@ def magic_timeit(line='', local_ns=None):
     setupstmt = '\n'.join([x[1] for x in opts if x[0] == '-s'])
     stmt = '\n'.join(stmt)
     opts = dict(opts)
-    number = int(opts.get("n", 0))
+    number = int(opts.get("-n", 0))
     default_repeat = 7 if timeit.default_repeat < 7 else timeit.default_repeat
-    repeat = int(opts.get("r", default_repeat))
-    precision = int(opts.get("p", 3))
-    quiet, return_result = 'q' in opts, 'o' in opts
-    if 'c' in opts:
+    repeat = int(opts.get("-r", default_repeat))
+    precision = int(opts.get("-p", 3))
+    quiet, return_result = '-q' in opts, '-o' in opts
+    if '-c' in opts:
         timefunc = clock
     elif 't' in opts:
         timefunc = time.time
