d2658c32b33dc4ebf83425010d80f06671c5fe69
thatbirdguythatuknownot <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2021-07-01 22:44:53 +0800

    Added setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,19 @@ HERE = Path(__file__).parent
 README = (HERE/"README.md").read_text()
 setup(
     name="magic-commands",
-    version="0.0.1",
+    version="0.0.10",
     description="IPython magic commands, now available in pure Python",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/thatbirdguythatuknownot/magic-commands",
     author="Crowthebird",
-    author_email=,
-    ,
+    author_email="nohackingofkrowten@gmail.com",
+    license="AGPL-3.0",
+    classifiers=[
+        "License :: OSI Approved :: GNU Affero General Public License version 3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10"
+    ],
+    packages=["magiccmds"],
+    include_package_data=True
+)
