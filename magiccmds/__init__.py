9ab8251c9c1d07f8de6b70c1e60500c1fbfe5a65
thatbirdguythatuknownot <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2021-07-02 17:36:23 +0800

    Updated version

--- magiccmds/__init__.py
+++ magiccmds/__init__.py
@@ -1,4 +1,4 @@
 #__init__.py
 __all__ = ['timeit', 'time', 'prun', 'version']
 from .implementations import (magic_timeit as timeit, magic_time as time, magic_prun as prun)
-version = "0.0.13.1-2"
+version = "0.0.13.1-3"
