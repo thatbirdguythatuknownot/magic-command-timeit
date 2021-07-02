7202dd69dd34271ab95a18c746b23b8e643c52e4
thatbirdguythatuknownot <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2021-07-02 16:06:23 +0800

    Fixed versioning

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/thatbirdguythatuknownot/magic-commands",
-    download_url="https://github.com/thatbirdguythatuknownot/magic-commands/archive/refs/tags/v0.0.13.zip",
+    download_url="https://github.com/thatbirdguythatuknownot/magic-commands/archive/refs/tags/v0.0.13.1.zip",
     author="Crowthebird",
     author_email="nohackingofkrowten@gmail.com",
     license="AGPL-3.0",
